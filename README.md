6d410248c5006ba77df66b5b1e28e98b7d26cab4
Edvaldo-Jr <edvaldo-oliveirajunior@hotmail.com>
2021-09-14 00:49:51 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,5 @@
+**Codificação de um sistema de contagem e classificação.**
+
+*****Se o numero for par, escreva 'par' e o numero correspondente.
+*Se o numero for impar, escreva 'impar' e o numero correspondente.
+*Se o numero for zero, escreva 'zero' e o numero correspondente.
---
+++ saviniscontagem.js
@@ -0,0 +1,22 @@
+let alunos = 30;
+let i = 0;
+
+for(i; i <= alunos; i++) {
+  if (i == 0){
+    console.log("Numero zero: " +i);
+  }
+
+  else if(i % 2 == 0) {
+    console.log("Numero par: " +i);
+  }
+  
+        else {
+          console.log ("Numero impar: " +i);
+
+        }
+    
+
+}
+
+    
+
